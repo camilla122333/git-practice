bc1608c4c757186f4a1df6dbf79030421631c81f
camilla122333 <pasadenastringtheory@gmail.com>
2022-03-25 13:44:01 -0700

    Implemented function c

--- function_c.py
+++ function_c.py
@@ -3,8 +3,13 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
+    return list(set(list_a) | set(list_b))
 
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+
+# print(merge_lists(list1, list2))
 
 if __name__ == "__main__":
     print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
